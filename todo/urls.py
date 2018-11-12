b9b66f392fa7bd3943339757cdb59f6a53d0453e
Minsu Kang <kms920612@gmail.com>
2018-11-12 06:27:47 +0000

    CURD 완성

--- todo/urls.py
+++ todo/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('new/', views.new),
     path('create/', views.create),
     path('<int:id>/', views.read),
+    path('todo_create/', views.todo_create),
+    path('<int:id>/delete/', views.delete),
+    path('<int:id>/update/', views.update),
     ]
--- todo/views.py
+++ todo/views.py
@@ -17,6 +17,31 @@ def create(request):
     todo.save()
     return redirect('/todos/')
     
+def todo_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        deadline = request.POST.get('deadline')
+        # todo = Todo(title=title, deadline=deadline)
+        # todo.save()
+        Todo.objects.create(title=title, deadline=deadline)
+        return redirect('/todos/')
+    return render(request, 'todo/todo_create.html')
+    
 def read(request, id):
     todo = Todo.objects.get(id=id)
-    return render(request, 'todo/read.html', {'todo': todo})
+    deadline = todo.deadline.strftime("%Y-%m-%d")
+    return render(request, 'todo/read.html', {'todo': todo,'deadline':deadline})
+    
+def delete(request, id):
+    Todo.objects.get(id=id).delete()
+    return redirect('/todos/')
+    
+def update(request, id):
+    todo = Todo.objects.get(id=id)
+    deadline = todo.deadline.strftime("%Y-%m-%d")
+    if request.method == 'POST':
+        todo.title = request.POST.get('title')
+        todo.deadline = request.POST.get('deadline')
+        todo.save()
+        return redirect('/todos/')
+    return render(request, 'todo/update.html', {'todo': todo, 'deadline': deadline})
